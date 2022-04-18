820fc561dbaf0753d13308f576e1c2213a628973
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-04-19 00:58:08 +0300

    done task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,5 +1,6 @@
 // Напиши скрипт, который при потере фокуса на инпуте(событие blur),
-//     проверяет его содержимое на правильное количество введённых символов.
+//     проверяет его содержимое на правильное количество введённых
+//символов.
 
 // <input
 //   type="text"
@@ -12,3 +13,25 @@
 //     если неправильное - красным.
 // Для добавления стилей, используй CSS - классы valid и invalid,
 //     которые мы уже добавили в исходные файлы задания.
+const input = document.querySelector("#validation-input");
+
+const inputLength = input.dataset.length;
+
+input.addEventListener("blur", checkLengsInputValue);
+input.addEventListener("focus", removeClass);
+
+function removeClass() {
+  if (input.classList.contains("valid")) {
+    input.classList.remove("valid");
+  }
+  if (input.classList.contains("invalid")) {
+    input.classList.remove("invalid");
+  }
+}
+
+function checkLengsInputValue(event) {
+  const string = event.currentTarget.value;
+  if (string.length === Number(inputLength)) {
+    input.classList.add("valid");
+  } else input.classList.add("invalid");
+}
